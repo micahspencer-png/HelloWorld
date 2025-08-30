52a8db6c6a3722a2824414e6c307ed7e72044cf5
micahspencer-png <micahspencer@isu.edu>
2025-08-29 21:19:36 -0600

    Added header to program, and fixed the console read function

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace HelloWorld
+﻿//Micah Spencer
+//RCET2265
+//Fall 2025
+//Hello World Program
+//https://github.com/micahspencer-png/HelloWorld.git
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.Read();
         }
     }
 }
